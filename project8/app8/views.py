b0de1d6281ee5624997ffc0996d53e69ba95cb35
vishalkalasakar <vishalkalaskar143@gmail.com>
2021-06-09 18:24:19 +0530

    Initial commit

---
+++ project8/app8/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def loginpage(request):
+    return render(request, "loginpage.html")
+
+def succesfull(request):
+    username = request.POST.get("sname")
+    pas = request.POST.get("pass")
+
+    if username == "ravi" and pas == "kumar":
+         return render(request, "succesfull.html")
+
+    else:
+       return render(request, "error.html")
